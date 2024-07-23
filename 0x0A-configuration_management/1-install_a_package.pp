e20dcbcff03ad8ba686ee929b7d045073f83659e
buyekeobare <buyekeobare@gmail.com>
2024-07-23 12:29:45 -0700

    Flask installed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,18 +1,18 @@
 # Install flask from pip3 using puppet
 
 package {'python3.8':
-  ensure   -> '3.8.10',
-  provider -> 'pip3',
+  ensure   => '3.8.10',
+  provider => 'pip3',
 }
 
 package {'Flask',
-  ensure   -> '2.1.0',
-  provider -> 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
 package {'Werkzeug',
-  ensure   -> '2.1.1',
-  provider -> 'pip3',
-  require  -> Package['Flask'],
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],
 }
 
