4850ba3ed2bb7d1cd289093867e907eaafb04e4c
buyekeobare <buyekeobare@gmail.com>
2024-08-13 13:33:49 -0700

    0-subs.py updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,17 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Queries a subreddit and retrieves no. of subscribers"""
+
+     # Reddit API endpoint for getting subreddit informatiom
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        'User-Agent': 'My user Agent 1.0'
-    }
 
- response = requests.get(url, headers=headers, allow_redirects=False)
+    # Set a custom User-Agent to avoid too many requests error
+    headers = {'User-Agent': 'My user Agent 1.0'}
 
+    # send a GET request to the Reddit API
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Chek if the request was successful and not redirect
     if response.status_code == 200:
         # parse JSON response to extract no of subscribers
         data = response.json().get('data', {})
