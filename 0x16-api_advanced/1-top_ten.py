f41b888f6fcfe9d6cba1f73d4d62b019056e940e
buyekeobare <buyekeobare@gmail.com>
2024-08-13 13:46:18 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Quering subreddit"""
+"""Querying subreddit"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Queries Reddit and prints titles of the first 10 hot posts"""
+    """Query Reddit and print titles of the first 10 hot posts"""
 
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
 
-    # Set a custom User-Agent to avoid too many requests error
-    headers = {'User-Agent': 'My user Agent 1.0'}
+    # Set a custom User Agent to avoid too many request errors
+    headers = {'User-Agent': 'MyRedditApp/1.0 (by /u/buyekeobare)'}
 
     # send a GET request to the Reddit API
     response = requests.get(url, headers=headers, allow_redirects=False)
