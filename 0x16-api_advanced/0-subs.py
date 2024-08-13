d1b134d50954651e93820d2a3bde4fe228ea928e
buyekeobare <buyekeobare@gmail.com>
2024-08-13 13:28:21 -0700

    0-subs.py updated

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,21 +8,15 @@ def number_of_subscribers(subreddit):
     """Queries a subreddit and retrieves no. of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        'User-Agent': 'MyRedditApp/1.0 (by /u/Alogyn)'
+        'User-Agent': 'MyRedditApp/1.0 (by /u/buyekeobare)'
     }
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        print(f"Status Code: {response.status_code}")
-        if response.status_code == 403:
-            print("403 Forbidden: Access is denied.")
-            return 0
-        elif response.status_code != 200:
-            print(f"Unexpected Status Code: {response.status_code}")
-            return 0
+ response = requests.get(url, headers=headers, allow_redirects=False)
 
+    if response.status_code == 200:
+        # parse JSON response to extract no of subscribers
         data = response.json().get('data', {})
-        return data.get('subscribers', 0)
-    except Exception as e:
-        print(f"Error: {e}")
+        sub_count = data.get('subscribers', 0)
+        return sub_count
+    else:
         return 0
